cd5ed750aaf73a81c77ce31db6e1e6c4a0452795
Nikhitamanagooli <01fe22bca033@kletech.ac.in>
2024-05-03 15:01:45 +0530

    code changes to fruits array

---
+++ FruitsArray.java
@@ -0,0 +1,11 @@
+//array
+public class FruitsArray {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Banana", "Orange", "Grapes", "Mango"};
+
+        System.out.println("Array of Fruits:");
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
